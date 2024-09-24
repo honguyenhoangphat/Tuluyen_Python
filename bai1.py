238180de177f1a047962a3da84ce0322a1fc95a4
Phat <phatho0317@gmail.com>
2024-09-24 20:33:36 +0700

    Bai1

---
+++ bai1.py
@@ -0,0 +1,9 @@
+##Tinh va hien thi so co co xu ly dau vao
+
+try:
+    so1 = int(input("nhập số thứ nhất: "))
+    so2 = int(input("Nhập số thứ 2: "))
+    tong = so1 + so2
+    print("Vậy tổng hai số là: ", tong)
+except:
+    print("Dinh dang dau vao khong hop le")
